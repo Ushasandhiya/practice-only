21656c6de828360d23cb844bdf017e86dc6f647f
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-25 22:55:30 +0530

    yes

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+alert("Hello, World!");
+let name = "Sandy";
+let age = 19;
+let isStudent = true;
+console.log(typeof name);       
+console.log(typeof age);        
+console.log(typeof isStudent);  
+let username = prompt("Name:");
+let likesJs = confirm("yap yap")
+let message = "ss" + username + "!";
+if (likesJs) 
+  message += "\nohhh code !";
+  else {
+  message += "\nOh no! Why not?"
+}
+
+alert(message);
