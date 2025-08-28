8ea211ad16ca764e234d468367c7653f09bc6a10
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-28 22:11:50 +0530

    examples

--- pow.java
+++ pow.java
@@ -1,3 +1,13 @@
 public class pow {
+    public static void main(String[] args) {
+        int base = 2;
+        int exponent = 3;
+        int result = 1;
+        for(int i=1; i<=exponent; i++)
+        {
+            result = result * base;
+        }
+        System.out.println("Result: " + result);
+    }
     
 }
