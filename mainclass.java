17a3302c8007808fbd2b53af522f3da2ad02b4bb
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-28 21:35:07 +0530

    examples

---
+++ mainclass.java
@@ -0,0 +1,25 @@
+class person
+{
+    public String name;
+    protected int age;
+    private String socialSecuritynumber;
+    String address;
+
+    person(String name)
+    {
+        this.name = name;
+
+    }
+
+}
+
+class Emloyee extends per
+
+
+public class mainclass
+{
+    public static void main(String[] args) {
+        person p1 = new person("san");
+
+    }
+}
