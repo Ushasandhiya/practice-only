c037e66b5c94d55b2dbcb45aaa74707dc6bce1f9
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-28 21:43:18 +0530

    examples

--- san.java
+++ san.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 public class san {
+
     
+    public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int num = scan.nextInt();
+    int lastdigit = num % 10;
+    System.out.println("Last digit is: " + lastdigit);
+    int n = num;
+    while(n>=10)
+    {
+        n = n/10;
+        System.out.println(n);
+    }
+
+        
+    }
+
     
-}
+
