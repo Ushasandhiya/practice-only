d255b4316f38bec6d995345f99d767223ed63103
Ushasandhiya <sandhiya.pcy@gmail.com>
2025-08-28 21:57:06 +0530

    examples

---
+++ rev.java
@@ -0,0 +1,13 @@
+public class rev {
+
+    public static void main(String[] args) {
+        int num = 123456;
+        int remainder;
+        int rev = 0;
+        while(num!=0)
+        {
+            
+        }
+    }
+    
+}
